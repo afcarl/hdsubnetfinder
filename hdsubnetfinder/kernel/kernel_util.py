206a8d2bd6219dc7b53d93e25370f80754533c1a
keiono <keiono@gmail.com>
2015-08-18 13:33:10 -0700

    0.2.2 release.

--- hdsubnetfinder/kernel/kernel_util.py
+++ hdsubnetfinder/kernel/kernel_util.py
@@ -1,10 +1,8 @@
 # -*- coding: utf-8 -*-
+import cStringIO
 
 
-def write_kernel(target_kernel, output_file):
-    """
-    Serialize the kernel to the supplied output file
-    """
+def __validate(target_kernel):
     index2node = target_kernel.index2node
 
     if index2node is None:
@@ -13,7 +11,10 @@ def write_kernel(target_kernel, output_file):
     kernel = target_kernel.kernel
     labels = target_kernel.labels
 
-    output_stream = open(output_file, 'w')
+    return kernel, labels, index2node
+
+
+def __write(kernel, labels, index2node, output_stream):
 
     cx = kernel.tocoo()
     edges = {}
@@ -38,4 +39,25 @@ def write_kernel(target_kernel, output_file):
 
         output_stream.write(printstr + "\n")
 
+
+def get_kernel_as_string(target_kernel):
+    """
+    Return a kernel as a big string object.
+    """
+    output_stream = cStringIO.StringIO()
+    kernel, labels, index2node = __validate(target_kernel)
+    __write(kernel, labels, index2node, output_stream)
+    kernel_as_big_string = output_stream.getvalue()
+    output_stream.close()
+
+    return kernel_as_big_string
+
+
+def write_kernel(target_kernel, output_file):
+    """
+    Serialize the kernel to the supplied output file
+    """
+    kernel, labels, index2node = __validate(target_kernel)
+    output_stream = open(output_file, 'w')
+    __write(kernel, labels, index2node, output_stream)
     output_stream.close()
